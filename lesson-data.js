136c3c3ca398f2d3ab9707c1dbc9edd596e6cbfe
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-21 05:17:25 +0300

    Modify lesson data to add more videos for lesson 2
    Updated lesson 2 to include multiple video URLs.

--- lesson-data.js
+++ lesson-data.js
@@ -1,5 +1,4 @@
 let lessons={
 1:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/choo.mp4"],
-2:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4"],
-3:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/chanjo.mp4"],
+2:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/chanjo.mp4","https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4"],
 10:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/meno.mp4"]};
