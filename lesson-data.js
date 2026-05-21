19aaa9b927c0b9f22cecf3fd25f000161d830a35
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-21 05:55:58 +0300

    Refactor lessons object to use string keys

--- lesson-data.js
+++ lesson-data.js
@@ -1,4 +1,16 @@
-let lessons={
-1:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/choo.mp4"],
-2:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/chanjo.mp4","https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4"],
-15:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/meno.mp4"]};
+let lessons = {
+
+  "1": [
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/choo.mp4"
+  ],
+
+  "2": [
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/chanjo.mp4",
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4"
+  ],
+
+  "10": [
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/meno.mp4"
+  ]
+
+};
