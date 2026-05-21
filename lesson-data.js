ad06a497747d02372170ce322027264f78d52d83
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-21 23:20:26 +0300

    Revise lesson-data with new video URLs
    Updated lesson links and added a new lesson entry.

--- lesson-data.js
+++ lesson-data.js
@@ -1,10 +1,14 @@
 let lessons = {
 
   "1": [
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/afya.mp4"
+  ],
+  
+  "2": [
     "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/choo.mp4"
   ],
 
-  "2": [
+  "3": [
     "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/chanjo.mp4",
     "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4"
   ],
