0615d3d3f8cc4e0648a543a39b91bd88af9b7f38
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-22 06:03:38 +0300

    Update lesson data with new video links

--- lesson-data.js
+++ lesson-data.js
@@ -1,14 +1,23 @@
 let lessons = {
 
   "1": [
-    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/afya.mp4"
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/malaria 1.mp4",
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/malaria 2.mp4"
   ],
   
   "2": [
-    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/choo.mp4"
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/tb.mp4"
   ],
 
   "3": [
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/afya.mp4"
+  ],
+
+  "4": [
+    "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/choo.mp4"
+  ],
+
+  "5": [
     "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/chanjo.mp4",
     "https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4"
   ],
