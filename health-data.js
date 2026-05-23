496c98b9be5d8011d0829ce1509dcd2fa6b4456f
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-23 05:58:57 +0300

    Update health-data.js

--- health-data.js
+++ health-data.js
@@ -3,28 +3,39 @@ const healthyTopics = {
 hiv: {
   title: "VVU / UKIMWI (Darasa la Hospitali)",
   lines: [
+
     "📖 Ufafanuzi",
     "Virusi vya Ukimwi ni virusi vinavyoathiri mfumo wa kinga ya mwili na kudhoofisha uwezo wa kupambana na magonjwa.",
-    { type: "image", src: "https://i.imgur.com/0uaNI3y.jpeg" },
+
+    {
+      type: "image",
+      src: "https://derrickafricatz-wq.github.io/AFYA-CARE/images/a1.jpeg"
+    },
 
     "📖 Dalili",
     "Homa za mara kwa mara, uchovu mkali, kupungua uzito, na maambukizi ya mara kwa mara.",
 
     "📖 Njia za Maambukizi",
-    "Virusi Vya Ukimwi huenea kupitia ngono bila kinga, damu iliyoambukizwa, na matumizi ya sindano zisizo salama.",
-    { type: "image", src: "https://i.imgur.com/sdp3Co3.jpeg" },
+    "Virusi vya Ukimwi huenea kupitia ngono bila kinga, damu iliyoambukizwa, na matumizi ya sindano zisizo salama.",
+
+    {
+      type: "image",
+      src: "https://derrickafricatz-wq.github.io/AFYA-CARE/images/a2.jpeg"
+    },
 
     "📖 Kinga",
     "Tumia kondomu wakati wa ngono, epuka kushiriki sindano, na hakikisha damu ni salama kabla ya kupewa.",
 
     "📖 Matibabu",
-    "Kutumia dawa za kudhibiti makali ya virusi vya ukimwi zinazo muwezesha mtu kuishi maisha marefu na yenye afya.",
+    "Kutumia dawa za kudhibiti makali ya virusi vya ukimwi zinazo muwezesha mtu kuishi maisha marefu na yenye afya."
+
   ]
 },
 
 infectionSpread: {
   title: "Njia za Maambukizi ya Magonjwa",
   lines: [
+
     "📖 Ufafanuzi",
     "Maambukizi hutokea pale viini vya magonjwa vinapohama kutoka kwa mtu mmoja kwenda mwingine.",
 
@@ -33,58 +44,88 @@ infectionSpread: {
     "Kukohoa na kupiga chafya husambaza vimelea kwa urahisi.",
     "Kutonawa mikono ni chanzo kikubwa cha maambukizi.",
     "Kuepuka mikusanyiko wakati wa mlipuko hupunguza hatari ya kuambukizwa.",
-    { type: "image", src: "https://i.imgur.com/3m9ncCx.jpeg" },
+
+    {
+      type: "image",
+      src: "https://derrickafricatz-wq.github.io/AFYA-CARE/images/b.jpeg"
+    },
 
     "📖 Kinga",
-    "Osha mikono mara kwa mara, vaa barakoa wakati wa mlipuko, na epuka kugusana na wagonjwa.",
+    "Osha mikono mara kwa mara, vaa barakoa wakati wa mlipuko, na epuka kugusana na wagonjwa."
+
   ]
 },
 
 vaccines: {
   title: "Umuhimu wa Chanjo",
   lines: [
+
     "📖 Ufafanuzi",
     "Chanjo husaidia mwili kujenga kinga dhidi ya magonjwa kabla ya kuambukizwa.",
-    { type: "image", src: "https://i.imgur.com/oBItPMK.jpeg" },
+
+    {
+      type: "image",
+      src: "https://derrickafricatz-wq.github.io/AFYA-CARE/images/c.jpeg"
+    },
 
     "📖 Jinsi Chanjo Inavyofanya Kazi",
     "Chanjo hufundisha mfumo wa kinga kutambua na kupambana na vimelea vya magonjwa.",
 
     "📖 Umuhimu",
     "Watoto na watu wazima wanahitaji chanjo kulingana na umri wao.",
-    "Chanjo huzuia milipuko ya magonjwa kwenye jamii.",
+    "Chanjo huzuia milipuko ya magonjwa kwenye jamii."
+
   ]
 },
 
 respiratory: {
   title: "Magonjwa ya Njia ya Hewa",
   lines: [
+
     "📖 Ufafanuzi",
     "Magonjwa ya njia ya hewa huathiri mfumo wa kupumua na huenea kwa urahisi.",
 
     "📖 Dalili",
     "Kikohozi, homa, na kupumua kwa shida.",
-    { type: "image", src: "https://i.imgur.com/jKkFIiu.jpeg" },
+
+    {
+      type: "image",
+      src: "https://derrickafricatz-wq.github.io/AFYA-CARE/images/d.jpeg"
+    },
 
     "📖 Kinga",
     "Vaa barakoa wakati wa mlipuko na epuka msongamano.",
-    "Nawa mikono mara kwa mara na tumia sanitizer.",
+    "Nawa mikono mara kwa mara na tumia sanitizer."
+
   ]
 },
 
 medication: {
   title: "Matumizi Sahihi ya Dawa",
   lines: [
+
     "📖 Ufafanuzi",
     "Kutumia dawa kwa usahihi ni muhimu kwa afya na kuzuia usugu wa dawa.",
 
     "📖 Kanuni Muhimu",
     "Tumia dawa kulingana na maelekezo ya daktari.",
-    { type: "image", src: "https://i.imgur.com/DST5dyx.png" },
+
+    {
+      type: "image",
+      src: "https://derrickafricatz-wq.github.io/AFYA-CARE/images/e1.png"
+    },
+
     "Usitumie antibiotics bila ushauri wa mtaalamu wa afya.",
+
     "Kumaliza dozi kamili ni muhimu kuzuia usugu wa dawa.",
-    { type: "image", src: "https://i.imgur.com/kd0B9Dc.png" },
-    "Hifadhi dawa vizuri ili zisipoteze ufanisi wake.",
+
+    {
+      type: "image",
+      src: "https://derrickafricatz-wq.github.io/AFYA-CARE/images/e2.jpeg"
+    },
+
+    "Hifadhi dawa vizuri ili zisipoteze ufanisi wake."
+
   ]
 }
 
