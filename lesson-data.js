56c6609f150100fcb5505f9f31f4bfd292ee27e9
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-09-01 11:24:38 +0300

    Update lesson video URLs to new source

--- lesson-data.js
+++ lesson-data.js
@@ -1,7 +1,7 @@
 let lessons = {
-  "1": ["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/n1.mp4"],
-  "2": ["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/n2.mp4"],
-  "3": ["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/n3.mp4"],
-  "4": ["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/n4.mp4"],
-  "5": ["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/n5.mp4"]
+  "1": ["https://derrickafricatz-wq.github.io/mbingu-duniani/videos/n1.mp4"],
+  "2": ["https://derrickafricatz-wq.github.io/mbingu-duniani/videos/n2.mp4"],
+  "3": ["https://derrickafricatz-wq.github.io/mbingu-duniani/videos/n3.mp4"],
+  "4": ["https://derrickafricatz-wq.github.io/mbingu-duniani/videos/n4.mp4"],
+  "5": ["https://derrickafricatz-wq.github.io/mbingu-duniani/videos/n5.mp4"]
 };
