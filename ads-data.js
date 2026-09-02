60aa43464490b6ab1daaffc8879c602b65f5caf6
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-09-02 07:14:12 +0300

    Update adImages array with new image path

--- ads-data.js
+++ ads-data.js
@@ -1,2 +1,2 @@
-const adImages=["https://derrickafricatz-wq.github.io/AFYA-CARE/images/ad3.png",
-                "https://derrickafricatz-wq.github.io/AFYA-CARE/images/one.jpg"];
+const adImages=
+  ["images/b.jpg"];
