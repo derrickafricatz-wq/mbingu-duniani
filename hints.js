9975e51d5a7448f9fcb758d47c824f33a7428417
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-09-12 06:43:28 +0300

    Simplify lessonHints array in hints.js
    Removed specific lesson hints to simplify the list.

--- hints.js
+++ hints.js
@@ -1,11 +1,7 @@
 const lessonHints = [
-  "1 • malaria",
-  "2 • tb",
-  "3 • Afya",
-  "4 • Choo bora", 
-  "5 . Chanjo",
-  "5 . Usafi",
-  "10 • Kinywa",
-  "15 . Moyo",
-  "20 . clinic"
+  "1 •",
+  "2 •",
+  "3 •",
+  "4 •", 
+  "5 ."
 ];
